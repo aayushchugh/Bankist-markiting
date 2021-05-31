2d7fd0db25951f864ee5c547788e81336477518e
Ayush Chugh <ayushchugh2006@gmail.com>
2021-05-31 13:13:26 +0530

    Fade menu navigation

--- script.js
+++ script.js
@@ -4,14 +4,15 @@
 // Modal window
 
 const header = document.querySelector('.header');
+const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
-
-const section1 = document.querySelector('#section--1');
 
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
@@ -89,4 +90,26 @@ tabsContainer.addEventListener('click', function (e) {
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
+});
+
+// Fade menu navigation
+const handleHover = (e, opacity) => {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = opacity;
+    });
+    logo.style.opacity = opacity;
+  }
+};
+
+nav.addEventListener('mouseover', e => {
+  handleHover(e, 0.5);
+});
+
+nav.addEventListener('mouseout', e => {
+  handleHover(e, 1);
 });
